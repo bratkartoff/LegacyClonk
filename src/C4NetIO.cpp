b263a81c99d35900b4aa2949e67222971bc750ee
Fulgen301 <tokmajigeorge@gmail.com>
2019-07-24 22:54:13 +0200

    Prefer public/stable IPv6 source addresses
    We don't benefit at all from privacy addresses as we're publishing all
    local addresses in any case. By preferring stable addresses, we can
    avoid reconnection issues after the preferred address changes.
    
    This API is defined in RFC5014, but apparently only implemented in
    Linux.
    
    This is essentially the same as OpenClonk commit 181f7ab47150738a1104644c43fee23cf6e17463
    by Lukas Werling plus one small fix for a misplacement of EnableDualStack
    (which gets changed by this commit anyway).

--- src/C4NetIO.cpp
+++ src/C4NetIO.cpp
@@ -56,6 +56,7 @@
 #endif
 
 #ifdef __linux__
+#include <linux/in6.h>
 #include <linux/if_addr.h>
 
 // Linux definitions needed for parsing /proc/if_inet6
@@ -712,7 +713,7 @@ C4NetIO::C4NetIO()
 
 C4NetIO::~C4NetIO() {}
 
-bool C4NetIO::EnableDualStack(SOCKET socket)
+bool C4NetIO::InitIPv6Socket(SOCKET socket)
 {
 	int opt = 0;
 	if (setsockopt(socket, IPPROTO_IPV6, IPV6_V6ONLY, reinterpret_cast<char*>(&opt), sizeof(opt)) == SOCKET_ERROR)
@@ -720,6 +721,15 @@ bool C4NetIO::EnableDualStack(SOCKET socket)
 		SetError("could not enable dual-stack socket", true);
 		return false;
 	}
+
+#ifdef IPV6_ADDR_PREFERENCES
+	// Prefer stable addresses. This should prevent issues with address
+	// deprecation while a match is running. No error handling - if the call
+	// fails, we just take any address.
+	opt = IPV6_PREFER_SRC_PUBLIC;
+	setsockopt(socket, IPPROTO_IPV6, IPV6_ADDR_PREFERENCES, reinterpret_cast<char*>(&opt), sizeof(opt));
+#endif
+
 	return true;
 }
 
@@ -1101,10 +1111,9 @@ bool C4NetIOTCP::Connect(const C4NetIO::addr_t &addr) // (mt-safe)
 		return false;
 	}
 
-	if (!EnableDualStack(lsock))
-	{
-		return false;
-	}
+	if (addr.GetFamily() == HostAddress::IPv6)
+		if (!InitIPv6Socket(nsock))
+			return false;
 
 #ifdef STDSCHEDULER_USE_EVENTS
 	// set event
@@ -1434,6 +1443,10 @@ bool C4NetIOTCP::Listen(uint16_t inListenPort)
 		SetError("socket creation failed", true);
 		return false;
 	}
+
+	if (!InitIPv6Socket(lsock))
+		return false;
+
 	// To be able to reuse the port after close
 #if !defined(_DEBUG) && !defined(_WIN32)
 	int reuseaddr = 1;
@@ -1799,7 +1812,7 @@ bool C4NetIOSimpleUDP::Init(uint16_t inPort)
 		return false;
 	}
 
-	if (!EnableDualStack(sock))
+	if (!InitIPv6Socket(sock))
 	{
 		return false;
 	}
